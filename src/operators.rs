59cffe28f65a23a76f6e9164d846d4ef4ddbf215
Iscreamx <iscreamx404@gmail.com>
2025-01-06 15:12:12 +0800

    finished swiglu()

--- src/operators.rs
+++ src/operators.rs
@@ -77,13 +77,12 @@ pub fn rms_norm(y: &mut Tensor<f32>, x: &Tensor<f32>, w: &Tensor<f32>, epsilon: 
 // y = silu(x) * y
 // hint: this is an element-wise operation
 pub fn swiglu(y: &mut Tensor<f32>, x: &Tensor<f32>) {
-    // let len = y.size();
-    // assert!(len == x.size());
+    assert_eq!(y.size(), x.size());
 
-    // let _y = unsafe { y.data_mut() };
-    // let _x = x.data();
-
-    todo!("实现 silu，这里给了一些前期准备工作的提示，你可以参考")
+    let y = unsafe { y.data_mut() };
+    let x = x.data();
+    
+    y.iter_mut().zip(x).for_each(|(y_i, &x_i)| *y_i *= x_i / (1. + (-x_i).exp()));
 }
 
 // C = beta * C + alpha * A @ B^T
