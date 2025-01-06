89d8d1338d39300ecb02e26e8193f1f3d9d0959b
Iscreamx <iscreamx404@gmail.com>
2025-01-07 00:38:38 +0800

    finished mlp

--- src/model.rs
+++ src/model.rs
@@ -3,7 +3,7 @@ use std::vec;
 
 use crate::config::LlamaConfigJson;
 use crate::kvcache::KVCache;
-use crate::operators as OP;
+use crate::operators::{self as OP, matmul_transb, rms_norm, swiglu};
 use crate::params::LLamaParams;
 use crate::tensor::Tensor;
 use safetensors::SafeTensors;
@@ -167,7 +167,25 @@ fn mlp(
     rms_w: &Tensor<f32>,
     eps: f32,
 ) {
-    todo!("Implement mlp");
+    residual.print();
+    rms_norm(hidden_states, residual, rms_w, eps);
+    hidden_states.print();
+    matmul_transb(gate, 0., hidden_states, w_gate, 1.);
+    gate.print();
+    matmul_transb(up, 0., hidden_states, w_up, 1.);
+    up.print();
+    swiglu(up, gate);
+    up.print();
+    matmul_transb(hidden_states, 0., up, w_down, 1.);
+    hidden_states.print();
+    unsafe {
+        residual
+            .data_mut()
+            .iter_mut()
+            .zip(hidden_states.data())
+            .for_each(|(r, &h)| *r += h);
+    }
+    residual.print();
 }
 
 #[test]
