f8886e56fd2b7e28528f93757201412a722f6585
Iscreamx <iscreamx404@gmail.com>
2025-01-08 16:28:37 +0800

    modified self_att

--- src/model.rs
+++ src/model.rs
@@ -3,7 +3,7 @@ use std::vec;
 
 use crate::config::LlamaConfigJson;
 use crate::kvcache::KVCache;
-use crate::operators::{self as OP, matmul_transb, rms_norm, swiglu};
+use crate::operators::{self as OP, masked_softmax, matmul_transb, rms_norm, swiglu};
 use crate::params::LLamaParams;
 use crate::tensor::Tensor;
 use safetensors::SafeTensors;
@@ -153,7 +153,26 @@ fn self_attention(
     total_seq_len: usize,
     dqkv: usize,
 ) {
-    todo!("Implement self_attention");
+    // todo!("Implement self_attention");
+    for i in 0..n_kv_h {
+        let k_batch = k.slice(i * total_seq_len * dqkv, &vec![total_seq_len, dqkv]);
+        for j in 0..n_groups {
+            let q_batch = q.slice(i * j * seq_len * dqkv, &vec![seq_len, dqkv]);
+            let mut att_scores_slice = att_scores.slice(i * j * seq_len * total_seq_len, &vec![seq_len, total_seq_len]);
+            matmul_transb(&mut att_scores_slice, 0., &q_batch, &k_batch, 1.);
+        }
+    }
+
+    masked_softmax(att_scores);
+
+    for i in 0..n_kv_h {
+        let v_batch = v.slice(i * total_seq_len * dqkv, &vec![dqkv, total_seq_len]);
+        for j in 0..n_groups {
+            let att_scores_slice = att_scores.slice(i * j * seq_len * total_seq_len, &vec![seq_len, total_seq_len]);
+            let mut hidden_states_slice = hidden_states.slice(i * j * seq_len * dqkv, &vec![seq_len, dqkv]);
+            matmul_transb(&mut hidden_states_slice, 0., &att_scores_slice, &v_batch, 1.);
+        }
+    }
 }
 
 fn mlp(
